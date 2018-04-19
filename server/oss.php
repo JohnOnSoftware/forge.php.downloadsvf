eb013bd3576b5fbc5704c863be5dd97cc56c921a
Zhong Wu <wuzhong1980@gmail.com>
2018-04-19 15:30:32 +0800

    update

--- server/oss.php
+++ server/oss.php
@@ -4,8 +4,75 @@ use Autodesk\Forge\Client\Api\BucketsApi;
 use Autodesk\Forge\Client\Model\PostBucketsPayload;
 use Autodesk\Forge\Client\Api\ObjectsApi;
 use Autodesk\Forge\Client\Api\DerivativesApi;
+use Autodesk\Forge\Client\Model\Manifest;
+
+
+class PathInfo
+{
+  private  $RootFileName;
+  private  $LocalPath;
+  private  $BasePath;
+  private  $URN;
+  private  $Files;
+
+  public function __get($property_name){
+    if(isset($this->$property_name)){
+        return($this->$property_name);
+    }else{
+        return(NULL);
+    }      
+  }
+
+    public function __set($property_name, $value){
+        $this->$property_name = $value;
+    }
+};
+
+class ManifestItem{
+  private $Guid;
+  private $MIME;
+  private $Path;
+
+  public function __get($property_name){
+    if(isset($this->$property_name)){
+        return($this->$property_name);
+    }else{
+        return(NULL);
+    }      
+  }
+
+    public function __set($property_name, $value){
+        $this->$property_name = $value;
+    }
+
+};
+
 
 class DataManagement{
+
+    private $urns = array();
+    private static $ROLES = array(
+        "Autodesk.CloudPlatform.DesignDescription",
+        "Autodesk.CloudPlatform.PropertyDatabase",
+        "Autodesk.CloudPlatform.IndexableContent",
+        "leaflet-zip",
+        "thumbnail",
+        "graphics",
+        "preview",
+        "raas",
+        "pdf",
+        "lod"
+    );
+
+    public static $BASE_URL = 'https://developer.api.autodesk.com/';
+    public static $DERIVATIVE_PATH = "derivativeservice/v2/derivatives/";   
+    // public static $DERIVATIVE_PATH = "modelderivative/v2/designdata/";
+    
+    public static $urn = "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6cGhwc2FtcGxlYnVja2V0L3dvcmtzaG9wX2JpbV9waHAucnZ0";
+    
+    
+    
+
     public function __construct(){
         set_time_limit(0);
     }    
@@ -114,25 +181,175 @@ class DataManagement{
           }
       } 
 
+
+      public function DownloadSVF( ){
+        global $twoLeggedAuth;
+        $accessToken = $twoLeggedAuth->getTokenInternal();
+        $this->ExtractSVF(self::$urn, $accessToken);
+      }    
+
       private function ExtractSVF( $urn, $accessToken){
         $derivativeApi = new DerivativesApi( $accessToken);
         //$urn = "urn_example"; // string | The Base64 (URL Safe) encoded design URN
         $accept_encoding = "accept_encoding_example"; // string | If specified with `gzip` or `*`, content will be compressed and returned in a GZIP format.
         
         try {
-            $result = $derivativeApi->getManifest($urn);
-            print_r($result);
+            $Manifest = $derivativeApi->getManifest($urn);
+            $this->ParseManifest($Manifest['derivatives']);
+            // var_dump(($this->urns));
+
+            foreach($this->urns as $key=>$item){
+                // if($item->MIME != 'application/autodesk-f2d' && $key>40 && $key<50){
+                //     continue;
+                // }
+                switch($item->MIME){
+                    case "application/autodesk-svf":
+                        //$item->Path->Files = $this->SVFDerivates($item, $accessToken->getAccessToken());
+                        break;
+                    case "application/autodesk-f2d":
+                        $item->Path->Files = $this->F2DDerivates($item, $accessToken->getAccessToken());
+                        break;
+                    case "application/autodesk-db":
+                        $item->Path->Files = array(
+                            "objects_attrs.json.gz",
+                            "objects_vals.json.gz",
+                            "objects_offs.json.gz",
+                            "objects_ids.json.gz",
+                            "objects_avs.json.gz",
+                            $item->Path->RootFileName
+                         );
+                        break;
+                  default:
+                    $item->Path->Files = array(
+                        $item->Path->RootFileName
+                    );
+                    break;
+                }
+
+            }
+            // var_dump(($this->urns));
+            
+
         } catch (Exception $e) {
             echo 'Exception when calling DerivativesApi->getManifest: ', $e->getMessage(), PHP_EOL;
         }
 
-    }
-      public function DownloadSVF( ){
-        global $twoLeggedAuth;
-        $accessToken = $twoLeggedAuth->getTokenInternal();
-        $urn = "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6cGhwc2FtcGxlYnVja2V0L3dvcmtzaG9wX2JpbV9waHAucnZ0";
-        $this->ExtractSVF($urn, $accessToken);
-      }    
+      }
+
+      private function GetDerivative($manifest, $accessToken){
+          // prepare to download the manifest
+        // Get cURL resource
+        $curl = curl_init();
+        // Set some options - we are passing in a useragent too here
+        $endpoint = self::$BASE_URL . self::$DERIVATIVE_PATH . $manifest;
+        $token = "authorization: Bearer " . $accessToken;
+        curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $endpoint,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                $token,
+                "Accept-Encoding: gzip, deflate",
+              ),
+          ));
+        // Send the request & save response to $resp
+        $resp = curl_exec($curl);
+        if( !$resp ){
+            echo curl_error($ch);
+        }
+        // Close request to clear up some resources
+        curl_close($curl);
+        var_dump($manifest);
+        var_dump($resp);
+
+        // $ret = stream_read($resp);
+        $content;
+        while( !streamWrapper::stream_eof($resp)){
+            $content = $content . streamWrapper::stream_read($resp);
+        }
+        
+        // $content = stream_get_contents($resp);
+        var_dump($content);
+        die;
 
 
+        // $header_array = get_headers($endpoint, true);
+        // $size = $header_array['Content-Length'];//获取远程app的大小
+        // header("Content-type: application/octet-stream");
+        // header("Accept-Encoding:  gzip, deflate");
+        // header( $token );
+        // header('Content-Disposition: attachment; filename="' . basename($endpoint) . '"');
+        // header("Content-Length: ".$size);//. filesize($file)
+        // readfile($endpoint);
+
+        ///////////////////////////
+        // $derivativeApi = new DerivativesApi( $accessToken);
+        // $derivativeApi->apiClient
+      
+      }
+
+      private function SVFDerivates($ManifestItem, $accessToken){
+        $manifest = $this->GetDerivative($ManifestItem->Path->URN, $accessToken);
+
+        $files = array();
+        array_push($files, $MainifestItem->Path->BasePath); // add the BasePath
+        array_push($files, GetAssets($manifest));
+
+        return $files;
+      }
+
+      private function F2DDerivates($ManifestItem, $accessToken){
+        $manifest = $this->GetDerivative($ManifestItem->Path->BasePath . "manifest.json.gz", $accessToken);
+        $files = array();
+        array_push($files, "manifest.json.gz");
+        array_push($files, GetAssets($manifest));
+
+        return $files;
+      }
+
+      private function GetAssets($mainfest){
+          // TBD
+        // List<string> files = new List<string>();
+
+        // // for each "asset" on the manifest, add to the list of files (skip embed)
+        // foreach (JObject asset in manifest["assets"])
+        // {
+        // System.Diagnostics.Debug.WriteLine(asset["URI"].Value<string>());
+        // if (asset["URI"].Value<string>().Contains("embed:/")) continue;
+        // files.Add(asset["URI"].Value<string>());
+        // }
+
+        // return files;          
+      }
+
+      private function ParseManifest( $manifest ){
+        foreach($manifest as $item ){
+            if( $item['role'] &&  (in_array($item['role'], self::$ROLES)) ){
+                $manifestItem = new ManifestItem();
+                $manifestItem->Guid = $item['guid'];
+                $manifestItem->MIME = $item['mime'];
+                $manifestItem->Path = $this->DecomposeURN($item['urn']);
+
+                array_push($this->urns, $manifestItem);
+            }
+            if( $item['children']){
+                $this->ParseManifest($item['children']);
+            }
+        }
+      }
+      
+      // Decompose the URN to local files
+      private function DecomposeURN( $encodedURN ){
+        $urn = str_replace('"', '\"', $encodedURN);
+        $path = new PathInfo();
+        $path->URN = $encodedURN;
+        $path->RootFileName = substr(strrchr($urn, "/"), 1);
+        $path->BasePath     = substr($urn, 0, strrpos($urn, "/")+1 );
+        $path->LocalPath    = substr($path->BasePath, strpos($path->BasePath, "/")+1 );
+        //   $path->LocalPath    = str_replace( "[/]?output/", "", $path->LocalPath );
+        //TBD How to replace "/" ?   
+        $path->LocalPath    = preg_replace("/output/", "", $path->LocalPath);
+
+        return $path;
+      }
 }
